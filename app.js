d62706872d5ff99fe51042a62a2d385b68b7a1b0
Kylie Smaldon <k_smaldon@outlook.com>
2022-11-22 16:13:31 +0000

    added fetch request and proxy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import router from "./routes/index.js"
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(morgan("dev"));
 //app.use(express.static("./public"));
