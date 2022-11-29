ef7887d95404a4df980e77cf3367d0a357dfab5d
Muhammed Atyib Ali <aatyib@gmail.com>
2022-11-29 15:27:03 +0000

    cypress testing complete

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import questionsRouter from "./routes/questions.js"
 import topicsRouter from "./routes/topics.js"
 const app = express();
 const PORT = 4000;
-
+//  var corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+app.use(cors())
 app.use(morgan("dev"));
 //app.use(express.static("./public"));
 app.use(express.json());
